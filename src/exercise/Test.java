622473ec03bd0db5a70a6f1dede2361de850c16d
1liuyao <2724324143@qq.com>
2023-02-16 22:53:06 +0800

    贪心：划分字母区间

--- src/exercise/Test.java
+++ src/exercise/Test.java
@@ -1,54 +1,28 @@
 package exercise;
 
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Test {
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        while (in.hasNextLine()) {// 注意，如果输入是多个测试用例，请通过while循环处理多个测试用例
-            String a = in.nextLine();
-            String b = in.nextLine();
+        int[] nums = new int[]{2, 3, 1, 5};
+        sort(nums);
 
-            int[] nums1 = new int[Integer.parseInt(a)];
-            int[] nums2 = new int[Integer.parseInt(b)];
-
-            String s1 = in.nextLine();
-            String[] s = s1.split(" ");
-
-            String s2 = in.nextLine();
-            String[] ss = s2.split(" ");
-
-            for (int i = 0; i < nums1.length; i++) {
-                nums1[i] = Integer.parseInt(s[i]);
-            }
-            for (int i = 0; i < nums2.length; i++) {
-                nums2[i] = Integer.parseInt(ss[i]);
-            }
-
-            int sum = getSum(nums1, nums2);
-            System.out.println(sum);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
         }
-
     }
 
-    public static int getSum(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> hashtable = new HashMap<>();
-        int sum = 0;
-        for (int i = 0; i < nums1.length; i++) {
-            if (hashtable.containsKey(nums1[i])) {
-                hashtable.put(nums1[i], hashtable.get(nums1[i]) + 1);
-            } else {
-                hashtable.put(nums1[i], 1);
+    private static void sort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
             }
         }
-
-        for (int i = 0; i < nums2.length; i++) {
-            if (hashtable.containsKey(nums2[i]))
-                sum += hashtable.get(nums2[i]);
-        }
-        return sum;
     }
-
 
 }
