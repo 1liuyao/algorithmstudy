6ee898b2d643c11881201b2e2f688b045f5a21b4
1liuyao <2724324143@qq.com>
2023-03-07 20:33:56 +0800

    单调栈 接雨水

---
+++ src/stack/test/NextGreaterElementsTest.java
@@ -0,0 +1,13 @@
+package stack.test;
+
+import stack.algorithm.NextGreaterElements;
+
+public class NextGreaterElementsTest {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,1};
+
+        NextGreaterElements nextGreaterElements = new NextGreaterElements();
+
+        nextGreaterElements.nextGreaterElements(nums);
+    }
+}
